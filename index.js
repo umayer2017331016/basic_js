f0bf50bf19429b2bf0e098867e6436efb53e074a
unknown <umayercse16@gmail.com>
2022-03-01 15:48:51 +0600

    basic Javascript

---
+++ index.js
@@ -0,0 +1,3 @@
+var price = 10;
+var buy = 8;
+console.log(price/buy);
---
+++ object.js
@@ -0,0 +1,5 @@
+var student = {id:1016, phone:1729992308, name:"Sariya"};
+student.result = "Aplus"; 
+console.log(student);
+console.log(student.name="lazu");
+console.log(student);
